70734b9eaea3489966365ef06f3ce1b76ba3af2c
Mark Crawley <mark@crawleyweb.co.uk>
2021-11-30 12:20:15 +0000

    Made Footer FooterMenu in line with Vue style guide

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,6 @@
   <div class="container">
     <MainMenu />
     <Nuxt keep-alive :keep-alive-props="{ max: 10, include: ['MainMenu', 'Footer'] }" />
-    <Footer />
+    <FooterMenu />
   </div>
 </template>
