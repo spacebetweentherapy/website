86df197252ec91a31637c254a5462e58d5134ae9
Mark Crawley <mark@crawleyweb.co.uk>
2021-12-01 11:41:58 +0000

    Keep left of image visible on shrink

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -98,7 +98,7 @@ export default Vue.extend({
     margin-right: auto;
     width: 100%;
     height: 100%;
-    object-position: 50% 50%;
+    object-position: 15% 50%;
     object-fit: cover;
   }
 }
