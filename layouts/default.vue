3f5b5e5296b366378163f81dc110390c283b8fa5
Mark Crawley <mark@crawleyweb.co.uk>
2022-01-09 10:44:20 +0000

    Remove pointless keep-alive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <MainMenu />
-    <Nuxt keep-alive :keep-alive-props="{ max: 10, include: ['MainMenu', 'Footer'] }" />
+    <Nuxt />
     <FooterMenu />
   </div>
 </template>
