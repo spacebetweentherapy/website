d172a37e9552300f8972d7fa6d9b584a218773f2
Mark Crawley <mark@crawleyweb.co.uk>
2024-01-06 10:22:50 +0000

    More homepage work

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from 'next'
-import { fetchGraphQL } from'./lib/cms'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import { fetchGraphQL } from './lib/cms'
+import Link from "next/link";
 import Hero from './components/hero'
 import Image from './components/image'
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import Quote from './components/quote'
+import '../styles/home.scoped.scss'
 
 export async function fetchPages() {
   const pages = await fetchGraphQL(`query {
@@ -37,12 +40,27 @@ export async function fetchPages() {
                   description
                 }
               }
+              quote {
+                sys {
+                    id
+                }
+                text
+                author
+                image {
+                  contentType
+                  url
+                  width
+                  height
+                  description
+                }
+              }
           }
       }
   }`)
   return pages?.data?.pageCollection?.items
 }
 
+// A hardcoded image for the homepage
 export async function fetchHomepageImage() {
   const image = await fetchGraphQL(`query {
     asset (id: "4QKYgZ75hpQq30yOokKwVj") {
@@ -53,7 +71,35 @@ export async function fetchHomepageImage() {
   }`)
   return image?.data.asset
 }
- 
+
+export async function fetchHomeMenuItems() {
+  const menuItems = await fetchGraphQL(`query {
+      homeMenuCollection {
+          items {
+              sys {
+                  id
+              }
+              linksCollection {
+                  items {
+                      sys {
+                          id
+                      }
+                      title
+                      slug
+                      thumbnailText
+                      thumbnail {
+                        url
+                        contentType
+                        description
+                     }
+                  }
+              }
+          }
+      }
+  }`)
+  return menuItems?.data?.homeMenuCollection?.items[0].linksCollection.items
+}
+
 export const metadata: Metadata = {
   title: 'Home - Space Between Therapy',
 }
@@ -63,34 +109,35 @@ export default async function Page() {
   const page = pages[0]
 
   const homepageImage = await fetchHomepageImage()
+  const homeMenu = await fetchHomeMenuItems()
 
   return (
     <div className="page page-home">
       <Hero
         heading={page.hero.heading}
         shortText={page.hero.shortText}
-        url={page.hero.image.url} 
-        contentType={page.hero.image.contentType} 
-        width={page.hero.image.width} 
-        height={page.hero.image.height} 
-        description={page.hero.image.description} 
-        className="hero__image"/>
+        url={page.hero.image.url}
+        contentType={page.hero.image.contentType}
+        width={page.hero.image.width}
+        height={page.hero.image.height}
+        description={page.hero.image.description}
+        className="hero__image" />
 
       <section key={page.sys.id} className="intro">
         <div className="intro__content">
-          { page.title ?
+          {page.title ?
             <h1 v-if="title" className="intro__title">
               {page.title}
             </h1>
-          : ''}
+            : ''}
 
-          { page.intro ?
+          {page.intro ?
             <div className="intro__text">
               <div>
                 <MDXRemote source={page.intro} />
               </div>
 
-              { page.introImage.url ?
+              {page.introImage.url ?
                 <Image
                   url={page.introImage.url}
                   width="200"
@@ -101,11 +148,11 @@ export default async function Page() {
                 />
                 : ''}
             </div>
-          : ''}
+            : ''}
 
-          { page.intro ?
-            <hr className="intro__line"/>
-          : ''}
+          {page.intro ?
+            <hr className="intro__line" />
+            : ''}
         </div>
       </section>
 
@@ -126,9 +173,48 @@ export default async function Page() {
           </div>
 
           {/* The style attribute is specific to the home page */}
-          <div className="content-wrapper" style={{padding:0}} v-html="markup" />
+          <div className="content-wrapper" style={{ padding: 0 }} v-html="markup" />
         </div>
       </section>
+
+      {/* Page specific content here */}
+      <div>
+        <div />
+        <section className="homepage-menu">
+          <div className="links">
+            <ul className="links__list">
+              {homeMenu.map((link) => (
+                <li key="link.sys.id" className="links__item">
+                  <Link href="link.slug" className="links__link">
+                    <Image url={link.thumbnail.url} contentType={link.thumbnail.contentType} description={link.thumbnail.description} width="200" height="200" x2={true} />
+                    <h2 className="links__title">
+                      {link.title}
+                    </h2>
+                    <p className="links__text">
+                      {link.thumbnailText}
+                    </p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <section className="contact">
+          <div className="content-main">
+            <div className="content-wrapper">
+              <p className="contact__text">
+                If you would like to arrange a free consultation, check my availability or ask a question, please feel free to get in touch.
+              </p>
+              <p className="button-container">
+                <a className="button" href="/contact">Contact</a>
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <Quote text={page.quote.text} author={page.quote.author} imgUrl={page.quote.image.url} imgWidth={page.quote.image.width} imgHeight={page.quote.image.height} imgDescription={page.quote.image.description} imgContentType={page.quote.image.contentType} />
     </div>
   )
 }
