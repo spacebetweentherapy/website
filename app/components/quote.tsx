b2b5375b9f894f2903ba0d220c1b7cdbbfd733ea
Mark Crawley <mark@crawleyweb.co.uk>
2024-01-06 10:23:06 +0000

    More homepage work

---
+++ app/components/quote.tsx
@@ -0,0 +1,17 @@
+import Image from './image'
+
+export default async function Quote({text, author, imgUrl, imgWidth, imgHeight, imgDescription, imgContentType}) {
+   return (
+        <div className="quote">
+            <div className="quote__wrapper">
+            <div className="quote__overlay">
+                <p className="quote__text">
+                <span className="quote__quote-mark">&ldquo;</span>{text}<span className="quote__quote-mark">&rdquo;</span>
+                </p>
+                <p className="quote__author">{author}</p>
+            </div>
+            <Image url={imgUrl} width={imgWidth} height={imgHeight} contentType={imgContentType} description={imgDescription} className="quote__image" />
+            </div>
+        </div>
+    )
+}
